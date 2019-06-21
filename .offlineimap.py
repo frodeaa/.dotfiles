5ef764f85bcbe67bbb3229ffd96389c6a0b576e5
Frode Aannevik <frode.aa@gmail.com>
2019-06-22 00:38:39 +0200

    brew: install and configure offlineimap
    sync
    
      INBOX -> INBOX
      [Gmail].All Mail -> archive
      [Gmail].Sent Mail -> sent
    
    for both accounts

---
+++ .offlineimap.py
@@ -0,0 +1,17 @@
+from subprocess import check_output
+
+def pass_store(key):
+    return check_output(["pass", "show", key]).strip()
+
+def gmail_nametrans(foldername):
+    return {
+        '[Gmail]/Sent Mail': 'sent',
+        '[Gmail]/All Mail': 'archive',
+    }.get(foldername, foldername)
+
+def gmail_folderfilter(foldername):
+    return foldername in [
+        'INBOX',
+        '[Gmail]/All Mail',
+        '[Gmail]/Sent Mail',
+    ]
